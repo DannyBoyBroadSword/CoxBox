888367ee1f134ec805f8bd76cfcaadfc8ae5bafa
DannyBoyBroadSword <andrewhennessy3455@gmail.com>
2016-11-21 14:05:40 -0500

    SKIRT
    ADD

--- app/server.js
+++ app/server.js
@@ -3,17 +3,6 @@ var five = require("johnny-five");
 var pitft = require("pitft");
 var sleep = require('sleep');
 
-var fb = pitft("/dev/fb1"); // Returns a framebuffer in direct mode.  See the clock.js example for double buffering mode
-
-// Clear the screen buffer
-fb.clear();
-
-fb.font("fantasy", 12); // Use the "fantasy" font with size 12
-
-var xMax = fb.size().width;
-var yMax = fb.size().height;
-
-fb.color(1, 1, 1); // Set the color to white
 
 
 //fb.font("fantasy", 12); // Use the "fantasy" font with size 12
@@ -27,6 +16,18 @@ var board = new five.Board({
 });
 
 board.on("ready", function() {
+  var fb = pitft("/dev/fb1"); // Returns a framebuffer in direct mode.  See the clock.js example for double buffering mode
+
+  // Clear the screen buffer
+  fb.clear();
+
+  fb.font("fantasy", 12); // Use the "fantasy" font with size 12
+
+  var xMax = fb.size().width;
+  var yMax = fb.size().height;
+
+  fb.color(1, 1, 1); // Set the color to white
+
   console.log("this might work");
 
   var gps = new five.GPS({
