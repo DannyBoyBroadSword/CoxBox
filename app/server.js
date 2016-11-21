1ceae05c407d82d2b9e2b23b0ef68732f26343bc
DannyBoyBroadSword <andrewhennessy3455@gmail.com>
2016-11-21 14:11:32 -0500

    dab

--- app/server.js
+++ app/server.js
@@ -3,6 +3,17 @@ var five = require("johnny-five");
 var pitft = require("pitft");
 var sleep = require('sleep');
 
+var fb = pitft("/dev/fb1",true); // Returns a framebuffer in direct mode.  See the clock.js example for double buffering mode
+
+// Clear the screen buffer
+fb.clear();
+
+fb.font("fantasy", 12); // Use the "fantasy" font with size 12
+
+var xMax = fb.size().width;
+var yMax = fb.size().height;
+
+fb.color(1, 1, 1); // Set the color to white
 
 
 //fb.font("fantasy", 12); // Use the "fantasy" font with size 12
@@ -16,18 +27,6 @@ var board = new five.Board({
 });
 
 board.on("ready", function() {
-  var fb = pitft("/dev/fb1"); // Returns a framebuffer in direct mode.  See the clock.js example for double buffering mode
-
-  // Clear the screen buffer
-  fb.clear();
-
-  fb.font("fantasy", 12); // Use the "fantasy" font with size 12
-
-  var xMax = fb.size().width;
-  var yMax = fb.size().height;
-
-  fb.color(1, 1, 1); // Set the color to white
-
   console.log("this might work");
 
   var gps = new five.GPS({
@@ -39,7 +38,6 @@ board.on("ready", function() {
   // If latitude, longitude, course or speed change log it
 
   gps.on("change", function() {
-    fb.clear();
     console.log("position");
     console.log("  latitude   : ", this.latitude);
     fb.text(20,20,"bruh",true);
@@ -48,8 +46,6 @@ board.on("ready", function() {
     fb.font("fantasy", 12); // Use the "fantasy" font with size 12
     fb.text(0, 20, this.longitude, false); // Draw the text non-centered, rotated _a_ degrees
     console.log("--------------------------------------");
-    sleep.sleep(5)
-
-
+    fb.blit();
   });
 });
