7f5df7e98ff7a1c20fef05f53c5bf0bd683e671c
DannyBoyBroadSword <andrewhennessy3455@gmail.com>
2016-11-22 15:51:09 -0500

    other stuff. everything else implemented except touch and control buttons.

--- app/server.js
+++ app/server.js
@@ -63,47 +63,30 @@ board.on("ready", function() {
     breakout: "ADAFRUIT_ULTIMATE_GPS",
     pins: ['P1-8', 'P1-10']
   });
+
   /*
-  imu.on("change", function() {
+  var volumecontrol = new five.i2c({
+    controller: "i2c"
+  });
 
-    //console.log("Thermometer");
-    //console.log("  celsius      : ", this.thermometer.celsius);
-    //console.log("  fahrenheit   : ", this.thermometer.fahrenheit);
-    //console.log("  kelvin       : ", this.thermometer.kelvin);
-    //console.log("--------------------------------------");
-
-    //console.log("Accelerometer");
-    //console.log("  x            : ", this.accelerometer.x);
-    //console.log("  y            : ", this.accelerometer.y);
-    //console.log("  z            : ", this.accelerometer.z);
-    //console.log("  pitch        : ", this.accelerometer.pitch);
-    //console.log("  roll         : ", this.accelerometer.roll);
-    //console.log("  acceleration : ", this.accelerometer.acceleration);
-    //console.log("  inclination  : ", this.accelerometer.inclination);
-    //console.log("  orientation  : ", this.accelerometer.orientation);
-    //console.log("--------------------------------------");
-
-    console.log("Gyroscope");
-    //console.log("  x            : ", this.gyro.x);
-    //console.log("  y            : ", this.gyro.y);
-    //console.log("  z            : ", this.gyro.z);
-    console.log("  pitch        : ", this.gyro.pitch);
-    console.log("  roll         : ", this.gyro.roll);
-    console.log("  yaw          : ", this.gyro.yaw);
-    //console.log("  rate         : ", this.gyro.rate);
-    //console.log("  isCalibrated : ", this.gyro.isCalibrated);
-    console.log("--------------------------------------");
+  var strokeRate = new five.Board({
+    controller: "arduino"
   });
   */
 
   imu.on("change", function() {
-    console.log("position");
+    console.log("GPS Data");
     console.log("  Speed   : ", gps.speed);
     console.log("  Course  : ", gps.course);
     console.log("  latitude   : ", gps.latitude);
     console.log("  longitude  : ", gps.longitude);
     console.log("  altitude   : ", gps.altitude);
-    console.log("  pitch        : ", imu.gyro.pitch);
+    console.log("MPU6050 Roll Data");
+    console.log("  pitch   : ", imu.gyro.pitch);
+    //console.log("Volume");
+    //console.log("  Volume   : ",volumecontrol.currentVolume);
+    //console.log("Stroke Rate");
+    //console.log("  Stroke Rate   : ",strokeRate.currentStrokeRate);
     console.log("--------------------------------------");
 
   });
