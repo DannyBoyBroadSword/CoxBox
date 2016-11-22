1c9e0a8c42c2f943a0ab738c3e9bb6cf1f060316
DannyBoyBroadSword <andrewhennessy3455@gmail.com>
2016-11-22 13:50:19 -0500

    STUFF
    DDD

--- app/server.js
+++ app/server.js
@@ -2,28 +2,28 @@ var Raspi = require("raspi-io");
 var five = require("johnny-five");
 var pitft = require("pitft");
 var sleep = require('sleep');
-var events = require('events');
-
 
 var fb = pitft("/dev/fb1"); // Returns a framebuffer in direct mode.  See the clock.js example for double buffering mode
 var xMax = fb.size().width;
 var yMax = fb.size().height;
-var sendBuffer = new events.EventEmitter();
-
-
-sendBuffer.on('data', function(){
-   fb.clear();
-   fb.text(0,50,"yes",true);
-});
-
-// Fire the connection event
-
-
+var latitude;
+var longitude;
 // Clear the screen buffer
 fb.clear();
 fb.color(1, 1, 1); // Set the color to whit
 fb.font("fantasy", 12); // Use the "fantasy" font with size 12
 
+fb.text(0,0,"Latitude:",false);
+fb.text(20,0,"Longitude:",false);
+fb.text(40,0,"Speed:",false);
+fb.text(60,0,"Course:",false);
+fb.text(80,0,"Longitude:",false);
+fb.text(100,0,"Balance:",false);
+fb.text(120,0,"Volume:",false);
+fb.text(140,0,"Stroke Rate:",false);
+fb.text(100,0,"/500:",false);
+fb.text(100,0,"Race Mode: 5000m",false);
+fb.text(100,0,"Estimated Finish time:",false);
 //fb.font("fantasy", 12); // Use the "fantasy" font with size 12
 //fb.text(0, 20, "HI", false); // Draw the text non-centered, rotated _a_ degrees
 //sleep.sleep(5);
@@ -34,14 +34,12 @@ var board = new five.Board({
   io: new Raspi()
 });
 
-/*
 function displayPush(){
     console.log("displayPush");
     fb.clear();
     fb.text(0,50,latitude,true);
     fb.text(50,50,longitude,true)     // The function returns the product of p1 and p2
 }
-*/
 
 board.on("ready", function() {
 
@@ -53,13 +51,9 @@ board.on("ready", function() {
   });
 
   gps.on("change", function() {
-    fb.clear();
     console.log("position");
     console.log("  latitude   : ", this.latitude);
-    fb.text(0,50,this.latitude,true);
     console.log("  longitude  : ", this.longitude);
-    fb.text(50,50,this.latitude,true);
     console.log("--------------------------------------");
-    //sendBuffer.emit('data');
   });
 });
