10f0798e8924bb7422f594afcd3e6b702c397b41
DannyBoyBroadSword <andrewhennessy3455@gmail.com>
2016-11-22 17:43:13 -0500

    everything

--- app/server.js
+++ app/server.js
@@ -2,8 +2,15 @@ var Raspi = require("raspi-io");
 var five = require("johnny-five");
 var pitft = require("pitft");
 var sleep = require('sleep');
+//var pitftTouch = require('pitft-touch');
+
+var WriteVolumeAddress=0x4B;
+var ReadStrokeAddress=8;
+var volumeLevel=31;
+var isStarted=false;
 
 var fb = pitft("/dev/fb1"); // Returns a framebuffer in direct mode.  See the clock.js example for double buffering mode
+//var touchscreen = pitftTouch("/dev/input/touchscreen");
 
 var xMax = fb.size().width;
 console.log(xMax);
@@ -16,38 +23,7 @@ fb.clear();
 fb.color(1, 1, 1); // Set the color to whit
 fb.font("fantasy", 12); // Use the "fantasy" font with size 12
 
-fb.text(0,20,"Latitude:",false);
-fb.text(0,40,"Longitude:",false);
-fb.text(0,60,"Speed:",false);
-fb.text(0,80,"Course:",false);
-fb.text(0,100,"Longitude:",false);
-fb.text(0,120,"Balance:",false);
-fb.text(0,140,"Volume:",false);
-fb.text(0,160,"Stroke Rate:",false);
-fb.text(0,180,"/500:",false);
-fb.text(0,200,"Race Mode: 5000m",false);
-fb.text(0,220,"Estimated Finish time:",false);
-
-sleep.sleep(5);
-/*
-sleep.sleep(5)
-
-fb.clear();
-fb.text(0,20,"Latitude: ya",false);
-fb.text(0,40,"Longitude: boy",false);
-fb.text(0,60,"Speed: the ",false);
-fb.text(0,80,"Course: lazy ",false);
-fb.text(0,100,"Longitude: brown",false);
-fb.text(0,120,"Balance: cow",false);
-fb.text(0,140,"Volume: jumped",false);
-fb.text(0,160,"Stroke Rate: over",false);
-fb.text(0,180,"/500: your ",false);
-fb.text(0,200,"Race Mode: 5000m mom",false);
-fb.text(0,220,"Estimated Finish time: lol",false);
-*/
-//fb.font("fantasy", 12); // Use the "fantasy" font with size 12
-//fb.text(0, 20, "HI", false); // Draw the text non-centered, rotated _a_ degrees
-//sleep.sleep(5);
+fb.text(0,20,"This will never work.",false);
 
 console.log("Starting Board");
 
@@ -56,24 +32,89 @@ var board = new five.Board({
 });
 
 board.on("ready", function() {
+  //Prepare i2c Data
+  new five.Pin('SDA');
+
+  //prepare buttons from TFT
+  var buttonOne = new five.Button(0);
+  var buttonTwo = new five.Button(3);
+  var buttonThree = new five.Button(4);
+  var buttonFour = new five.Button(2);
+
+
+  //prepare imu
   var imu = new five.IMU({
     controller: "MPU6050"
   });
 
+  //prepare gps
   var gps = new five.GPS({
     breakout: "ADAFRUIT_ULTIMATE_GPS",
     pins: ['P1-8', 'P1-10']
   });
 
-  /*
-  var volumecontrol = new five.i2c({
-    controller: "i2c"
+  //create a function that reads the Volume.
+  function readVolume(address){
+    return defaultVolumeLevel;
+  }
+
+  //create a cunction that writes Volume
+  function writeVolume(address,volume){
+    return board.io.i2cwrite(address,volume)
+    //sample writeVolume(WriteVolumeAddress,defaultVolumeLevel);
+  }
+
+  //create a function that read's stroke rate.
+  function readStrokeRate(address){
+    return board.io.i2cread(address,3,true);
+    //sample readStrokeRate(ReadStrokeAddress);
+  }
+
+  //default volume
+  writeVolume(writeVolumeAddress,volumeLevel);
+
+  buttonOne.on("press", function() {
+    writeVolume(WriteVolumeAddress,VolumeLevel+1);
+    VolumeLevel = VolumeLevel+1
+    console.log( "Increased Volume" );
   });
 
-  var strokeRate = new five.Board({
-    controller: "arduino"
+  buttonTwo.on("press", function() {
+    writeVolume(writeVolumeAddress,VolumeLevel-1);
+    VolumeLevel = VolumeLevel-1
+    console.log( "Decreased Volume" );
   });
+
+  buttonThree.on("press", function() {
+    if (isStarted == false){
+      //startSomething();
+      console.log("Started");
+    }else{
+      //stopSomething();
+      console.log("Stopped");
+    }
+  });
+
+  buttonThree.on("hold", function() {
+    //resetTimer();
+    isStarted = false;
+    console.log( "Reset" );
+  });
+
+  buttonFour.on("press", function() {
+    console.log( "IDK fam" );
+    //doSumting();
+  });
+
+  /*
+  touchscreen.on("data.touch", function() {
+    var screenX = ((270 - 50) / (693 - 3307)) * data.x + 320;
+    var screenY = ((190 - 50) / (3220 - 996)) * data.y;
+    console.log("  screenX   : ", screenX);
+    console.log("  screenY  : ", screeny);
+  }
   */
+
 
   gps.on("change", function() {
     console.log("GPS Data");
@@ -85,16 +126,13 @@ board.on("ready", function() {
     console.log("MPU6050 Roll Data");
     console.log("  pitch   : ", imu.gyro.pitch);
     console.log("MPU6050 Starting Acceleration");
-    console.log("  x   : ", imu.accelerometer.x);
-    console.log("  y   : ", imu.accelerometer.y);
-    console.log("  z   : ", imu.accelerometer.z);
+    console.log("  Acceleration   : ", imu.accelerometer.acceleration);
     console.logt("MPU6050 Temp");
     console.log("  fahrenheit   : ", imu.thermometer.fahrenheit);
-    //console.log("Volume");
-    //console.log("  Volume   : ",volumecontrol.currentVolume);
-    //console.log("Stroke Rate");
-    //console.log("  Stroke Rate   : ",strokeRate.currentStrokeRate);
+    console.log("Volume");
+    console.log("  Volume   : ",volumeLevel);
+    console.log("Stroke Rate");
+    console.log("  Stroke Rate   : ",readStrokeRate(ReadStrokeAddress));
     console.log("--------------------------------------");
-
   });
 });
