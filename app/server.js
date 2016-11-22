627e0fd77c3062b5f55d8971e41fedbd56935120
DannyBoyBroadSword <andrewhennessy3455@gmail.com>
2016-11-22 14:56:09 -0500

    test444

--- app/server.js
+++ app/server.js
@@ -15,7 +15,7 @@ console.log(yMax);
 fb.clear();
 fb.color(1, 1, 1); // Set the color to whit
 fb.font("fantasy", 12); // Use the "fantasy" font with size 12
-/*
+
 fb.text(0,20,"Latitude:",false);
 fb.text(0,40,"Longitude:",false);
 fb.text(0,60,"Speed:",false);
@@ -27,7 +27,19 @@ fb.text(0,160,"Stroke Rate:",false);
 fb.text(0,180,"/500:",false);
 fb.text(0,200,"Race Mode: 5000m",false);
 fb.text(0,220,"Estimated Finish time:",false);
-*/
+sleep.sleep(5)
+fb.clear();
+fb.text(0,20,"Latitude: ya",false);
+fb.text(0,40,"Longitude: boy",false);
+fb.text(0,60,"Speed: the ",false);
+fb.text(0,80,"Course: lazy ",false);
+fb.text(0,100,"Longitude: brown",false);
+fb.text(0,120,"Balance: cow",false);
+fb.text(0,140,"Volume: jumped",false);
+fb.text(0,160,"Stroke Rate: over",false);
+fb.text(0,180,"/500: your ",false);
+fb.text(0,200,"Race Mode: 5000m mom",false);
+fb.text(0,220,"Estimated Finish time: lol",false);
 
 //fb.font("fantasy", 12); // Use the "fantasy" font with size 12
 //fb.text(0, 20, "HI", false); // Draw the text non-centered, rotated _a_ degrees
