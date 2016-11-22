e84d747e561e7a24a9ac0164048a64bf73846012
DannyBoyBroadSword <andrewhennessy3455@gmail.com>
2016-11-22 14:30:19 -0500

    jye
    adsf

--- app/server.js
+++ app/server.js
@@ -3,20 +3,21 @@ var five = require("johnny-five");
 var pitft = require("pitft");
 var sleep = require('sleep');
 
-var fb = pitft("/dev/fb1"); // Returns a framebuffer in direct mode.  See the clock.js example for double buffering mode
+var fb = pitft("/dev/fb1",true); // Returns a framebuffer in direct mode.  See the clock.js example for double buffering mode
+
 var xMax = fb.size().width;
 console.log(xMax);
 var yMax = fb.size().height;
 console.log(yMax);
-var latitude;
-var longitude;
+
+
 // Clear the screen buffer
 fb.clear();
 fb.color(1, 1, 1); // Set the color to whit
 fb.font("fantasy", 12); // Use the "fantasy" font with size 12
 
 fb.text(0,20,"Latitude:",false);
-fb.text(0,0,"Longitude:",false);
+fb.text(0,40,"Longitude:",false);
 fb.text(0,60,"Speed:",false);
 fb.text(0,80,"Course:",false);
 fb.text(0,100,"Longitude:",false);
@@ -26,6 +27,7 @@ fb.text(0,160,"Stroke Rate:",false);
 fb.text(0,180,"/500:",false);
 fb.text(0,200,"Race Mode: 5000m",false);
 fb.text(0,220,"Estimated Finish time:",false);
+
 //fb.font("fantasy", 12); // Use the "fantasy" font with size 12
 //fb.text(0, 20, "HI", false); // Draw the text non-centered, rotated _a_ degrees
 //sleep.sleep(5);
@@ -36,16 +38,7 @@ var board = new five.Board({
   io: new Raspi()
 });
 
-function displayPush(){
-    console.log("displayPush");
-    fb.clear();
-    fb.text(0,50,latitude,true);
-    fb.text(50,50,longitude,true)     // The function returns the product of p1 and p2
-}
-
 board.on("ready", function() {
-
-  console.log("this might work");
 
   var gps = new five.GPS({
     breakout: "ADAFRUIT_ULTIMATE_GPS",
@@ -53,7 +46,6 @@ board.on("ready", function() {
   });
 
   gps.on("change", function() {
-    fb.text(0,20,this.latitude,false);
     console.log("position");
     console.log("  latitude   : ", this.latitude);
     console.log("  longitude  : ", this.longitude);
